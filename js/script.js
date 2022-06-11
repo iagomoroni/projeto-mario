fce91d5fd1df2b728ab88784922bd7e6bef6bb53
iagomoroni <iagomoroni@icloud.com>
2022-06-11 17:05:49 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const pipe = document.querySelector('.pipe');
 const jump = () => {
     mario.classList.add('jump');
 
-    setTimeout(() => {        
+    setTimeout(() => {
+
         mario.classList.remove('jump');
     }, 500);
 }
@@ -12,23 +13,24 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');    
+    
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
-        mario.style.left = `${marioPosition}px`;
+        mario.style.bottom = `${marioPosition}px`;
 
-        mario.src='./images/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
 
         clearInterval(loop);
     }
 
-}, 10);
+}, 10)
 
 document.addEventListener('keydown', jump);
